4b47fdab490d66f81c6a0b0c3e52f629386f3499
su <2296887348@qq.com>
2020-10-28 16:31:22 +0800

    首页基础布局

--- live-stream/App.vue
+++ live-stream/App.vue
@@ -1,17 +1,29 @@
 <script>
-	export default {
-		onLaunch: function() {
-			console.log('App Launch')
-		},
-		onShow: function() {
-			console.log('App Show')
-		},
-		onHide: function() {
-			console.log('App Hide')
-		}
+export default {
+	onLaunch: function() {
+		console.log('App Launch');
+		//监听底部导航中间凸起按钮
+		uni.onTabBarMidButtonTap(() => {
+			console.log('点击中间按钮');
+		});
+	},
+	onShow: function() {
+		console.log('App Show');
+	},
+	onHide: function() {
+		console.log('App Hide');
 	}
+};
 </script>
 
 <style>
-	/*每个页面公共css */
+/*每个页面公共css */
+/* 引用自定义的iconfont样式 */
+@import url('./common/icon.css');
+/* 动画库 */
+/* @import url("./common/animate.css"); */
+/* 自定义样式库 */
+@import url('./common/free.css');
+/* 全局样式 */
+@import url('./common/common.css');
 </style>
---
+++ live-stream/components/common/tab.vue
@@ -0,0 +1,23 @@
+<template>
+	<view class="flex fixed-bottom bg-dark content-around align-stretch bg-hover-light" style="height: 70px;">
+		<navigator url="../../pages/index/index" open-type="switchTab" class="flex justify-center bg-dark">发现</navigator>
+		<view class="flex align-center justify-center bg-danger" style="width: 250rpx;"><image src="../../static/tabbar/min.png" style="width: 60px; height: 60px;"></image></view>
+		<navigator url="../../pages/my/my" open-type="switchTab" style="width: 250rpx;" class="flex align-center justify-center bg-primary">我的</navigator>
+	</view>
+</template>
+
+<script>
+export default {
+	data() {
+		return {};
+	}
+};
+</script>
+
+<style>
+	.content{
+		width: 100%;
+		height: 100%;
+		background: #5a5b5a;
+	}
+</style>
