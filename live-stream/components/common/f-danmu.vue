5281b60ded778582d7681863394997c8c7d18388
su <2296887348@qq.com>
2020-10-29 19:40:42 +0800

    发送弹幕

--- live-stream/components/common/f-danmu.vue
+++ live-stream/components/common/f-danmu.vue
@@ -2,7 +2,7 @@
 	<view style="position: fixed;bottom: 120rpx;left: 0;right: 0;">
 		<scroll-view scroll-y="true" style="width: 520rpx;height: 300rpx;" scroll-with-animation class="pl-3" :scroll-into-view="scrollIntoView">
 			<view :id="'danmu'+item.id" class="flex justify-start align-start rounded p-2 mb-2" style="background-color: rgba(255,255,255,0.125);" v-for="(item,index) in list" :key="index">
-				<text class="font-md text-danger">{{item.name}}</text>
+				<text class="font-md text-danger">{{item.name}}:</text>
 				<text class="font-md text-white">{{item.content}}</text>
 			</view>
 		</scroll-view>
@@ -18,18 +18,24 @@
 			}
 		},
 		mounted() {
-			let id = 1
-			setInterval(()=>{
-				this.list.push({
-					id:id,
-					name:'观众'+id,
-					content:'发言_'+id
-				})
-				this.toBottom()
-				id++
-			},2000)
+			// let id = 1
+			// setInterval(()=>{
+			// 	this.list.push({
+			// 		id:id,
+			// 		name:'观众'+id,
+			// 		content:'发言_'+id
+			// 	})
+			// 	this.toBottom()
+			// 	id++
+			// },2000)
 		},
 		methods:{
+			//发送弹幕
+			send(data){
+				this.list.push(data)
+				//置于底部
+				this.toBottom()
+			},
 			toBottom(){
 				setTimeout(()=>{
 					let len = this.list.length
