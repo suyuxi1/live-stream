a8aec4427974042b1953bd2317743ce608b1cb31
su <2296887348@qq.com>
2020-10-30 15:00:36 +0800

    充值页面细化

--- live-stream/pages/coin/coin.vue
+++ live-stream/pages/coin/coin.vue
@@ -7,29 +7,129 @@
 			</view>
 
 			<view class="border-top border-light-secondary my-3"></view>
-		</view>
-		<view><text class="font-sm text-muted">请选择充值的金币</text></view>
-		<view class="flex flex-wrap" style="margin-left: -20rpx; margin-right: -20rpx;">
-			<view class="p-2" style="width: 33.3%; box-sizing: border-box;" v-for="i in 6">
-				<view class="border rounded flex flex-column align-center justify-center" style="height: 130rpx;">
-					<view class="flex align-center">
-						<text class="iconfont text-warning mr-1">&#xe633;</text>
-						<text class="font-md font-weight-bold">10</text>
+
+			<view><text class="font-sm text-muted">请选择充值的金币</text></view>
+
+			<view class="flex flex-wrap" style="margin-left: -20rpx; margin-right: -20rpx;">
+				<view class="p-2" style="width: 33.3%; box-sizing: border-box;" v-for="(item, index) in list" :key="index" @click="chooseCoin(index)">
+					<view
+						v-if="item.price > 0"
+						style="height: 130rpx;"
+						class="border rounded flex flex-column align-center justify-center"
+						:class="activeIndex === index ? 'border-main' : ''"
+					>
+						<view class="flex align-center">
+							<text class="iconfont text-warning mr-1">&#xe633;</text>
+							<text class="font-md font-weight-bold">{{ item.coin }}</text>
+						</view>
+						<text class="font text-light-muted">¥{{ item.price }}</text>
 					</view>
-					<text class="font text-light-muted">¥10</text>
+					<view v-else style="height: 130rpx;" class="border rounded flex flex-column align-center justify-center">
+						<text class="font text-light-muted">自定义</text>
+					</view>
 				</view>
 			</view>
+
+			<view class="position-fixed left-0 bottom-0 right-0 border-top flex px-3 align-center" style="height: 100rpx;">
+				<view class="flex align-center">
+					<text class="iconfont text-warning mr-1">&#xe633;</text>
+					<text class="font-md font-weight-bold">{{ price }}</text>
+				</view>
+				<view class="bg-main rounded flex align-center justify-center ml-auto" style="width: 150rpx; height: 70rpx;"><text class="text-white font-md">去充值</text></view>
+			</view>
+			<uni-popup ref="popup" type="dialog">
+				<uni-popup-dialog
+					mode="input"
+					message="自定义充值"
+					:duration="2000"
+					placeholder="充值金额"
+					inputType="number"
+					:before-close="true"
+					@close="close"
+					@confirm="confirm"
+				></uni-popup-dialog>
+			</uni-popup>
 		</view>
 	</view>
 </template>
 
 <script>
+import uniPopup from '../../components/uni-popup/uni-popup.vue';
+import uniPopupDialog from '../../components/uni-popup-dialog/uni-popup-dialog.vue';
 export default {
-	data() {
-		return {};
+	components: {
+		uniPopup,
+		uniPopupDialog
 	},
-	onLoad() {},
-	methods: {}
+	data() {
+		return {
+			activeIndex: 0,
+			list: [
+				{
+					coin: 10,
+					price: 10
+				},
+				{
+					coin: 20,
+					price: 20
+				},
+				{
+					coin: 30,
+					price: 30
+				},
+				{
+					coin: 40,
+					price: 40
+				},
+				{
+					coin: 50,
+					price: 50
+				},
+				{
+					coin: 100,
+					price: 100
+				},
+				{
+					price: 0
+				}
+			],
+			price: 0
+		};
+	},
+	onLoad() {
+		let p = this.list[this.activeIndex].price;
+		if (p > 0) {
+			this.price = p;
+		}
+	},
+	methods: {
+		chooseCoin(index) {
+			this.activeIndex = index;
+			let p = this.list[index].price;
+			if (p > 0) {
+				this.price = p;
+			} else {
+				//自定义价格
+				this.$refs.popup.open();
+			}
+		},
+		//点击取消按钮触发
+		close(done) {
+			done();
+		},
+		//点击确认触发
+		confirm(done, value) {
+			console.log(value);
+			if (!value) {
+				return uni.showToast({
+					title: '请输入要充值的金额',
+					icon: 'none'
+				});
+			}
+			this.price = value;
+			done();
+		}
+	}
 };
 </script>
 
