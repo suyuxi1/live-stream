df0fd2b1cc5fb187cdce5d57f2d994e37336ac77
su <2296887348@qq.com>
2020-11-01 14:12:08 +0800

    live-stream-api项目创建、跨域、全局异常处理

--- live-stream/pages/index/index.vue
+++ live-stream/pages/index/index.vue
@@ -52,7 +52,7 @@ export default {
 					gold: 10,
 					likes: 550,
 					title: '高清电影',
-					cover: 'http://b40.photo.store.qq.com/psb?/V146ck4w2VCSVT/m.TVJDKX64V7xGxmkf0LEmO8U8FZ.lc6mRYPTYD6zo8!/b/dCgAAAAAAAAA&bo=AASAAgAAAAAFB6I!&rf=viewer_4',
+					cover: 'http://pic.netbian.com/uploads/allimg/200922/224915-1600786155f16e.jpg',
 					url: 'http://ivi.bupt.edu.cn/hls/chchd.m3u8',
 					state: 0
 				},
@@ -60,7 +60,7 @@ export default {
 					gold: 10,
 					likes: 560,
 					title: '湖南卫视',
-					cover: 'http://b40.photo.store.qq.com/psb?/V146ck4w2VCSVT/m.TVJDKX64V7xGxmkf0LEmO8U8FZ.lc6mRYPTYD6zo8!/b/dCgAAAAAAAAA&bo=AASAAgAAAAAFB6I!&rf=viewer_4',
+					cover: 'http://pic.netbian.com/uploads/allimg/200803/000001-15963840010072.jpg',
 					url: 'http://ivi.bupt.edu.cn/hls/hunanhd.m3u8',
 					state: 1
 				},
@@ -68,7 +68,7 @@ export default {
 					gold: 10,
 					likes: 570,
 					title: '广东卫视',
-					cover: 'http://b40.photo.store.qq.com/psb?/V146ck4w2VCSVT/m.TVJDKX64V7xGxmkf0LEmO8U8FZ.lc6mRYPTYD6zo8!/b/dCgAAAAAAAAA&bo=AASAAgAAAAAFB6I!&rf=viewer_4',
+					cover: 'http://pic.netbian.com/uploads/allimg/200108/222203-1578493323c2d3.jpg',
 					url: 'http://ivi.bupt.edu.cn/hls/gdhd.m3u8',
 					state: 1
 				},
@@ -76,7 +76,7 @@ export default {
 					gold: 10,
 					likes: 580,
 					title: '金鹰纪实',
-					cover: 'http://b40.photo.store.qq.com/psb?/V146ck4w2VCSVT/m.TVJDKX64V7xGxmkf0LEmO8U8FZ.lc6mRYPTYD6zo8!/b/dCgAAAAAAAAA&bo=AASAAgAAAAAFB6I!&rf=viewer_4',
+					cover: 'http://pic.netbian.com/uploads/allimg/190328/223136-1553783496a5e9.jpg',
 					url: 'http://ivi.bupt.edu.cn/hls/gedocu.m3u8',
 					state: 0
 				},
@@ -84,7 +84,7 @@ export default {
 					gold: 10,
 					likes: 590,
 					title: '黑龙江',
-					cover: 'http://b40.photo.store.qq.com/psb?/V146ck4w2VCSVT/m.TVJDKX64V7xGxmkf0LEmO8U8FZ.lc6mRYPTYD6zo8!/b/dCgAAAAAAAAA&bo=AASAAgAAAAAFB6I!&rf=viewer_4',
+					cover: 'http://pic.netbian.com/uploads/allimg/181117/221319-1542463999f0fd.jpg',
 					url: 'http://ivi.bupt.edu.cn/hls/hljhd.m3u8',
 					state: 0
 				}
