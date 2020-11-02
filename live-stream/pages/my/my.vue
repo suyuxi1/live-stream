0d24971c2f7d65039c135dc9b29b890f66d67b87
su <2296887348@qq.com>
2020-11-02 16:04:02 +0800

    个人中心交互实现

--- live-stream/pages/my/my.vue
+++ live-stream/pages/my/my.vue
@@ -1,12 +1,27 @@
 <template>
 	<view>
 		<view class="top flex align-center justify-center"></view>
-		<view class="flex align-center">
+		<view v-if="!user" class="flex align-center">
+			<view class="flex align-center justify-center" style="width: 180rpx; height: 180rpx;">
+				<image src="../../static/logo.png" class="rounded-circle" style="width: 105rpx; height: 105rpx;" mode=""></image>
+			</view>
+			<view class="flex-column flex">
+				<text class="font-md">未登录</text>
+				<text class="font text-muted">登录体验更多功能</text>
+			</view>
+			<view class="ml-auto mr-3">
+				<view class="border border-main rounded flex align-center justify-center p-2" hover-class="bg-light" @click="openLogin">
+					<text class="text-main font">立即登录</text>
+				</view>
+			</view>
+		</view>
+
+		<view v-else class="flex align-center">
 			<view class="flex align-center justify-center position-relative" style="width: 180rpx; height: 160rpx;">
-				<image src="../../static/my.png" mode="" class="rounded-circle" style="width: 145rpx; height: 145rpx; position: absolute; top: -60rpx;"></image>
+				<image :src="user.avatar || '../../static/my.png'" mode="" class="rounded-circle" style="width: 145rpx; height: 145rpx; position: absolute; top: -60rpx;"></image>
 			</view>
 			<view class="flex flex-column">
-				<text class="font-md">Su</text>
+				<text class="font-md">{{ user.username }}</text>
 				<text class="font text-muted">疯狂男孩</text>
 			</view>
 			<view class="ml-auto mr-3">
@@ -19,12 +34,13 @@
 		<f-list-item icon="iconfaxian" title="我的关注"><text class="text-muted font">0</text></f-list-item>
 		<f-list-item icon="iconmore" title="历史记录"></f-list-item>
 		<view @click="toShare"><f-list-item icon="iconfenxiang" title="分享列表"></f-list-item></view>
-		<view @click="toLogin"><f-list-item icon="iconwode" title="我的登录"></f-list-item></view>
+		<view @click="logout"><f-list-item icon="iconwode" title="退出登录"></f-list-item></view>
 	</view>
 </template>
 
 <script>
 import fListItem from '../../components/common/f-list-item.vue';
+import { mapState } from 'vuex';
 export default {
 	components: {
 		fListItem
@@ -34,9 +50,15 @@ export default {
 			statusBarHeight: 0
 		};
 	},
+	computed: {
+		...mapState({
+			user: state => state.user
+		})
+	},
 	onLoad() {
 		let res = uni.getSystemInfoSync();
 		this.statusBarHeight = res.statusBarHeight;
+		this.$store.dispatch('getUserInfo');
 	},
 	methods: {
 		toShare() {
@@ -44,7 +66,18 @@ export default {
 				url: '../share/share'
 			});
 		},
-		toLogin() {
+		openLogin() {
+			uni.navigateTo({
+				url: '../login/login'
+			});
+		},
+		logout() {
+			this.$store.dispatch('logout').then(res => {
+				uni.showToast({
+					title: '退出成功',
+					icon: 'none'
+				});
+			});
 			uni.navigateTo({
 				url: '../login/login'
 			});
--- live-stream/store/index.js
+++ live-stream/store/index.js
@@ -1,15 +1,17 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+import $H from '../common/request.js'
+
 Vue.use(Vuex)
 
 export default new Vuex.Store({
 	state: {
-		test:111,
+		test: 111,
 		user: null,
 		token: null
 	},
-	action: {
+	actions: {
 		login({
 			state
 		}, user) {
@@ -18,6 +20,27 @@ export default new Vuex.Store({
 
 			uni.setStorageSync('user', JSON.stringify(user))
 			uni.setStorageSync('token', user.token)
+		},
+		logout({
+			state
+		}, user) {
+			state.user = null
+			state.token = null
+		},
+		getUserInfo({
+			state
+		}) {
+			$H.get('/user/info', {
+				token: true,
+				noJump: true,
+				toast: false
+			}).then(res => {
+				state.user = res
+				uni.setStorage({
+					key: "user",
+					data: JSON.stringify(state.user)
+				})
+			})
 		}
 	}
 })
